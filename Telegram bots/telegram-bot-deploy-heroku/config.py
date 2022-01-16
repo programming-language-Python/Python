ac483ba15f1e0d65f5cb0f064d5b60b40f970215
Rom1987 <maks3564195@gmail.com>
2022-01-16 19:42:17 +0300

    16.01.2022 deploy heroku

--- Telegram bots/telegram-bot-deploy-heroku/config.py
+++ Telegram bots/telegram-bot-deploy-heroku/config.py
@@ -1,3 +1,8 @@
-BOT_TOKEN = "1778886123:AAGdD9k1VlIsZR4Q2xLXaC5vhjaztrP_zdM"  # Your token
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')  # Your token
 URL_APPLES = "https://rozetka.com.ua/champion_a00225/p27223057"
 URL_PEAR = "https://freshmart.com.ua/product/yabloko-gala-116.html"
